13e8848b4de66504e870e63028c60b3564a4c6c0
Sushant Mathur <sushant.post@gmail.com>
2017-04-04 19:10:14 +0530

    Tests running!

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/AsyncTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/AsyncTest.java
@@ -1,10 +1,25 @@
 package com.udacity.gradle.builditbigger.tests;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.test.AndroidTestCase;
+import android.util.Pair;
+
+import com.udacity.gradle.builditbigger.RetrieveJokesTask;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by sushant on 4/4/17.
  */
 
 public class AsyncTest extends AndroidTestCase {
+    @Test
+    public void test_async() throws ExecutionException, InterruptedException {
+        String hello = new RetrieveJokesTask().execute(new Pair<Context, String>(InstrumentationRegistry.getContext(),"doesn't matter")).get();
+        String to_compare = "Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.";
+        assertEquals(to_compare,hello);
+    }
 }
